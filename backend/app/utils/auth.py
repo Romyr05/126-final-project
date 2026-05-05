887e71fc06cc4805c85e46ee19418fd2e5a1d311
romyr <romyrlopez123@gmail.com>
2026-05-05 15:00:58 +0800

    chore: added auth and added slug in supabase

---
+++ backend/app/utils/auth.py
@@ -0,0 +1,25 @@
+from typing import Annotated
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from app.database import supabase
+
+
+security = HTTPBearer()
+
+def get_current_user(credentials: Annotated[HTTPAuthorizationCredentials,Depends(security)]):
+    token = credentials.credentials 
+
+
+    try:
+        response = supabase.auth.get_current_user(token)
+        user = response.user   #this is because json file ni so need i indicate what value lng
+
+
+        if not user:
+            raise HTTPException(status_code=401, detail="Invalid authentication token")
+
+    except Exception:
+            raise HTTPException(status_code=401, detail="Could not validate user")
+        
+
+
