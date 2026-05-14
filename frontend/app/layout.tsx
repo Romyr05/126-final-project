14ddcde8eff6f276c944abeb4137392a104be9ca
romyr <romyrlopez123@gmail.com>
2026-05-15 02:16:20 +0800

    chore: sign-up pages and log-in

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,3 +1,4 @@
+import "./globals.css";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 
---
+++ frontend/components/sign-up/signup-form.tsx
@@ -0,0 +1,133 @@
+"use client"
+
+import { FormEvent} from "react"
+import { signup } from "@/lib/auth"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { useRouter } from "next/navigation"
+import React, { useState } from "react"
+
+
+
+export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
+  const router = useRouter()
+  const[error,setError] = useState("")
+
+  const[username, setUsername] = useState("")
+  const[email,setEmail] = useState("")
+
+  const[password,setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+
+  const [loading, setLoading] = useState(false)
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>){
+    event.preventDefault()
+    setError("")
+  
+    //Logic for the pasword
+
+    if(password !== confirmPassword){
+      setError(
+        "Password not match"
+      )
+      return
+    }
+
+    setLoading(true)
+
+    try{
+      const checking = await signup({
+        username,email,password
+      })
+      
+      //For not to run every edit or what
+      if (checking){
+        //The main screen
+        router.replace("/")
+      }
+    } catch{
+      setError("Could not create account")
+    } finally{
+      setLoading(false)
+    }
+
+
+  }
+
+
+  return (
+    <Card {...props}>
+      <CardHeader>
+        <CardTitle>Create an account</CardTitle>
+        <CardDescription>
+          Enter your information below to create your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit}>
+          <FieldGroup>
+            <Field>
+              <FieldLabel htmlFor="name">Full Name</FieldLabel>
+              <Input id="name" type="text" placeholder="John Doe" value={username}
+              onChange={(event) => setUsername(event.target.value)} required />
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="email">Email</FieldLabel>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                required
+              />
+              <FieldDescription>
+                We&apos;ll use this to contact you. We will not share your email
+                with anyone else.
+              </FieldDescription>
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="password">Password</FieldLabel>
+              <Input id="password" type="password" value = {password} onChange={(event) => setPassword(event.target.value)} required />
+              <FieldDescription>
+                Must be at least 8 characters long.
+              </FieldDescription>
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="confirm-password">
+                Confirm Password
+              </FieldLabel>
+              <Input id="confirm-password" type="password" required 
+               value = {confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)} />
+              <FieldDescription>Please confirm your password.</FieldDescription>
+            </Field>
+            <FieldGroup>
+              <Field>
+                <Button type="submit" disabled={loading}>
+                  {loading ? "Creating..." : "Create Account"}
+                </Button>
+                <FieldDescription className="px-6 text-center">
+                  Already have an account? <a href="#">Sign in</a>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
+  )
+}
