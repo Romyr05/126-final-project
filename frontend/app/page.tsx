12600ecfec2ac2d3f9ea857b8322abe061429e74
romyr <romyrlopez123@gmail.com>
2026-05-22 05:15:03 +0800

    chore: test run in landing page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,18 @@
+import Header from "@/components/layout/Header"
+import Footer from "@/components/layout/Footer"
+
 export default function Page() {
   return (
-    <div>
-      <h1>Hello, Next.js! TEST CI CD</h1>
+    <div className="flex min-h-screen flex-col">
+
+      <Header></Header>
+
+      <main className="flex-1">
+        <h1>Hello, Next.js! TEST CI CD</h1>
+      </main>
+
+      <Footer ></Footer>
+
     </div>
   )
 }
