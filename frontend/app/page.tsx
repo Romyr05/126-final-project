ed5eadfff018d72f9c5addacfc899cc1a65c4a11
romyr <romyrlopez123@gmail.com>
2026-05-22 23:16:38 +0800

    chore: prevent merge conflict in here

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,9 @@
-import Header from "@/components/layout/Header"
-import Footer from "@/components/layout/Footer"
-
 export default function Page() {
   return (
-    <div className="flex min-h-screen flex-col">
-
-      <Header></Header>
-
-      <main className="flex-1">
+    <div>
+      <main>
         <h1>Hello, Next.js! TEST CI CD</h1>
       </main>
-
-      <Footer ></Footer>
-
     </div>
   )
 }
