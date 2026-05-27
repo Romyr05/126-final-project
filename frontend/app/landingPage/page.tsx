4d9c0ef4a917f7abd3795b7549bb63f4e4e9b5b9
ultra <166610445+ultramarine23@users.noreply.github.com>
2026-05-27 17:24:40 +0800

    added api helper func in getting games

--- frontend/app/landingPage/page.tsx
+++ frontend/app/landingPage/page.tsx
@@ -12,17 +12,50 @@ import ImageOverlayCard from "@/components/landingPage/ImageOverlayCard"
 import { mockUserReviews } from "@/components/landingPage/mockUserReviews";
 import { mockGames } from "@/components/landingPage/mockGames"
 
+import { getGames } from "@/lib/api";
+
+
+type ApiGame = {
+  game_id: string
+  igdb_id: number
+  title: string
+  description: string | null
+  release_year: number | null
+  external_rating: number | null
+  avg_user_rating: number | null
+  cover_image: string | null
+  created_at: string
+  updated_at: string
+  slug?: string | null
+}
+
+type GetGamesRes = {
+  count: number
+  games: ApiGame[]
+}
 
 
 
 
+export default async function LandingPage() {
+  const data =
+    await getGames<GetGamesRes>(20, 0);
 
-export default function LandingPage() {
+  // transform api games
+  const games = data.games.map(game => ({
+    title: game.title,
+
+    // hardcoded for now
+    genres: ["Action"],
+
+    image: game.cover_image
+  }));
+
   return (
     <div>
       <ImageOverlayCard image = "/images/LandingPageImageOverlay.jpg" text = "Dummy text"></ImageOverlayCard>
       
-      <GameCardCatalog games={mockGames}></GameCardCatalog>
+      <GameCardCatalog games={games}></GameCardCatalog>
 
       <div className="bg-zinc-900 px-6 py-10 mt-10">
         <GameReviewHeader />
